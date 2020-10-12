17409f39ffa873f07d2e15fb1df9a66458c6aac3
arecki3000 <ark.kwi@gmail.com>
2020-10-12 09:42:38 +0200

    Updates

---
+++ precache-manifest.a6c09dbe058ac4c769255e7381a823b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05369bd906f13e505c5159c4ee852710",
+    "url": "/whatever-movie-app/index.html"
+  },
+  {
+    "revision": "20d84a3aebf63b05a7c5",
+    "url": "/whatever-movie-app/static/js/2.9dd6512e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/whatever-movie-app/static/js/2.9dd6512e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda9da0dc533aebc271b",
+    "url": "/whatever-movie-app/static/js/main.e53ab0c7.chunk.js"
+  },
+  {
+    "revision": "f1fdd693f0fcc424b51b",
+    "url": "/whatever-movie-app/static/js/runtime-main.63421125.js"
+  },
+  {
+    "revision": "00e6ed0b85c726c3cf908a14e20149a6",
+    "url": "/whatever-movie-app/static/media/Whatevericon.00e6ed0b.svg"
+  },
+  {
+    "revision": "77ec86236ebc3ec506f4baab606d32c1",
+    "url": "/whatever-movie-app/static/media/Whateverlogo.77ec8623.svg"
+  },
+  {
+    "revision": "1c89ac9f179f5dc3f4d9a9b56a9f2d98",
+    "url": "/whatever-movie-app/static/media/close.1c89ac9f.svg"
+  },
+  {
+    "revision": "157c5ef6891e3ff2b9dad348f7888e42",
+    "url": "/whatever-movie-app/static/media/facebook.157c5ef6.svg"
+  },
+  {
+    "revision": "d1b4253f16e6f9d9cf9cc5a0af03da24",
+    "url": "/whatever-movie-app/static/media/film-poster-placeholder.d1b4253f.png"
+  },
+  {
+    "revision": "00b5c87de549bbd7ebc429f673491c1e",
+    "url": "/whatever-movie-app/static/media/instagram.00b5c87d.svg"
+  },
+  {
+    "revision": "70dcdf29e95792421a14cb11142adae6",
+    "url": "/whatever-movie-app/static/media/linkedin.70dcdf29.svg"
+  },
+  {
+    "revision": "760ba975b6afc696075d944c9714a8a9",
+    "url": "/whatever-movie-app/static/media/menu.760ba975.svg"
+  },
+  {
+    "revision": "2e0c0ac5c4b8af91d783c2be3cfe306f",
+    "url": "/whatever-movie-app/static/media/movie-placeholder.2e0c0ac5.png"
+  },
+  {
+    "revision": "1dc0f0dc98aa4c2985762fd49b9e0fbc",
+    "url": "/whatever-movie-app/static/media/twitter.1dc0f0dc.svg"
+  }
+]);
